ca95baf1e38c0fb48bfbc62b1bdb8042cbace8f1
Niemiah <117840352+Niemiah@users.noreply.github.com>
2023-07-13 19:39:51 -0600

    Commit for my branch of the repository.

--- src/main/java/com/solvd/naviapp/main.java
+++ src/main/java/com/solvd/naviapp/main.java
@@ -1,21 +1,20 @@
 package com.solvd.naviapp;
-
-import java.util.Scanner;
 import com.solvd.naviapp.user.impl.UserInterface;
-import com.solvd.naviapp.bin.Client;
-import com.solvd.naviapp.bin.Graph;
-import com.solvd.naviapp.bin.Node;
-import com.solvd.naviapp.bin.Path;
-import com.solvd.naviapp.controller.services.INavService;
-import com.solvd.naviapp.controller.services.impl.NavService;
-import com.solvd.naviapp.db.implMyBatis.services.DbService;
-import com.solvd.naviapp.db.IDbService;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import java.util.Scanner;
+
+
+
+
 
 public class main {
     private static final Logger LOGGER = LogManager.getLogger(main.class);
 
     public static void main(String[] args) {
+        UserInterface userInterface = new UserInterface();
+        Scanner scanner = new Scanner(System.in);
+
+        userInterface.start();
     }
 }
